b5cb32480c40bb805e4baf425d567e8df9162c10
ManjuAkther <manjuakther47@gmail.com>
2025-08-19 15:29:54 +0600

    added this section

--- ticket.js
+++ ticket.js
@@ -1,20 +1,24 @@
 // Constants
 const seatPrice = 550;
 const maxSelectableSeats = 4;
-        function scrollToSection() {
-            document.getElementById('seat-booking').scrollIntoView({ behavior: 'smooth' });
+ function scrollToSection() {
+    document.getElementById('seat-booking').scrollIntoView({ behavior: 'smooth' });
         }
-
+function scrollToForm() {
+    document.getElementById('section2').scrollIntoView({ behavior: 'smooth' });
+        }
 // Select all seat elements
 const seatElements = document.querySelectorAll('.seat');
-const totalPriceElement = document.querySelectorAll('ol')[7]; // Grand Total
+
+const grandTotalPrice = document.querySelectorAll('ol')[12];
+const totalPriceElement = document.querySelectorAll('ol')[10]; // Total price
 const selectedSeatsList = document.querySelector('.seat-booking .seat-sel');
 
 // Track selected seats
 let selectedSeats = [];
-
 seatElements.forEach(seat => {
   seat.addEventListener('click', function() {
+    
     const seatNumber = seat.textContent;
     if (selectedSeats.includes(seatNumber)) {
       // Deselect seat
@@ -25,16 +29,22 @@ seatElements.forEach(seat => {
         // Select seat
         selectedSeats.push(seatNumber);
         seat.style.backgroundColor = '#4ade80'; // Tailwind green-400
-      } else {
+        
+      }
+       else {
         alert(`You can select up to ${maxSelectableSeats} seats only.`);
       }
     }
     updateTotal();
   });
-});
+
 
 function updateTotal() {
   // Update price display
+
   const total = selectedSeats.length * seatPrice;
   totalPriceElement.textContent = `BDT ${total}`;
-}
+  grandTotalPrice.textContent = `BDT ${total}`;
+  
+}});
+
